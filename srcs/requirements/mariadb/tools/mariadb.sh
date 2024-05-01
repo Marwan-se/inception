842af598f4b0edddd792784611e148023fd1e87d
marwankali <marwansekh123@gmail.com>
2024-05-01 11:54:58 -0400

    Inception

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,31 +1,18 @@
-#!/bin/sh
+#!bin/bash
 
-mysql_install_db
-
-service mariadb start
-
-if [ -d "/var/lib/mysql/$DB_NAME" ]; then
-    echo "Database exists alrdy"
-else
-    mysql_secure_installation << _EOF_
-Y
-rootabc
-rootabc
-Y
-n
-Y
-Y
-_EOF_
-
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWD' ;FLUSH PRIVILEGES;" | mariadb -uroot
-
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWD'; FLUSH PRIVILEGES;" | mariadb -u root
-
-
-fi
-
-mariadb-admin shutdown
 
 sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/"    /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mariadbd
+service mariadb start
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_USER_PASSWD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_USER_PASSWD}';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$DB_ROOT_PASSWD shutdown
